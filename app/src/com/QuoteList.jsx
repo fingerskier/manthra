e95907cbae46368c9063008d060f78d08279a7d1
Matt Currier <fingerskier@gmail.com>
2025-09-14 08:02:59 -0500

    placeholder

--- app/src/com/QuoteList.jsx
+++ app/src/com/QuoteList.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
+import {useLiveQuery} from 'dexie-react-hooks'
 
 
 export default function QuoteList() {
+  const quotes = useLiveQuery(() => db.quotes.toArray())
+
   return <ul>
-    <li>"The only way to do great work is to love what you do." - Steve Jobs</li>
-    <li>"Life is what happens when you're busy making other plans." - John Lennon</li>
-    <li>"Do not watch the clock. Do what it does. Keep going." - Sam Levenson</li>
-    <li>"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill</li>
-    <li>"Believe you can and you're halfway there." - Theodore Roosevelt</li>
-    <li>"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt</li>
-    <li>"You miss 100% of the shots you don't take." - Wayne Gretzky</li>
-    <li>"The best way to predict the future is to invent it." - Alan Kay</li>
-    <li>"I have not failed. I've just found 10,000 ways that won't work." - Thomas Edison</li>
-    <li>"It does not matter how slowly you go as long as you do not stop." - Confucius</li>
-    <li>"Everything you've ever wanted is on the other side of fear." - George Addair</li>
-    <li>"Hardships often prepare ordinary people for an extraordinary destiny." - C.S. Lewis</li>
-    <li>"Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle." - Christian D. Larson</li>
-    <li>"The only limit to our realization of tomorrow will be our doubts of today." - Franklin D. Roosevelt</li>
-    <li>"The purpose of our lives is to be happy." - Dalai Lama</li>
-    <li>"In the middle of every difficulty lies opportunity." - Albert Einstein</li>
-    <li>"Success usually comes to those who are too busy to be looking for it." - Henry David Thoreau</li>
-    <li>"Don't watch the clock; do what it does. Keep going." - Sam Levenson</li>
-    <li>"The harder the conflict, the greater the triumph." - George Washington</li>
-    <li>"What lies behind us and what lies before us are tiny matters compared to what lies within us." - Ralph Waldo Emerson</li>
+    {quotes && quotes.map(quote =>
+      <li key={quote.id}>
+        <blockquote>"{quote.text}"</blockquote>
+      </li>
+    )}
   </ul>
 }
