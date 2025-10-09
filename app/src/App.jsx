c01f5a9eaf856533ef4cd5725c8e7ef7c228ee3e
Matt Currier <fingerskier@gmail.com>
2025-10-08 21:24:49 -0500

    Randomize quotes display

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import {useMemo} from 'react'
 import Header from '@/Header'
 import {useLiveQuery} from 'dexie-react-hooks'
 import Quote from '@/Quote'
+import db from '@/db'
 
 import './App.css'
 
@@ -9,12 +10,23 @@ import './App.css'
 export default function App() {
   const quotes = useLiveQuery(() => db.quotes.toArray(), [])
 
+  const sortedQuotes = useMemo(() => {
+    if (!quotes) return []
+
+    const newQuotes = quotes.filter(quote => quote.text === 'A new quote')
+    const others = quotes.filter(quote => quote.text !== 'A new quote')
+
+    const shuffledOthers = others.slice().sort(() => Math.random() - 0.5)
+
+    return [...newQuotes, ...shuffledOthers]
+  }, [quotes])
+
   return <>
     <Header />
 
     <main>
       <ul>
-        {quotes?.map(quote => <Quote key={quote.id} data={quote} />)}
+        {sortedQuotes.map(quote => <Quote key={quote.id} data={quote} />)}
       </ul>
     </main>
   </>
